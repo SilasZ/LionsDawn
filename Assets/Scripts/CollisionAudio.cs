f45b9ed6dcde9f23569f26d9eafe35e539e8a730
SilasZ <silaszimmermann@yahoo.de>
2019-03-09 20:06:45 +0100

    collision and wind audio

---
+++ Assets/Scripts/CollisionAudio.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionAudio : MonoBehaviour
+{
+    public AudioSource sound;
+
+    void OnCollisionEnter2D(Collision2D collision)  //Plays Sound Whenever collision detected
+    {
+        sound.Play();
+    }
+}
