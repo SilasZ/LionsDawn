1f5b008311b6df28922b4edcd417db4ccb94d03a
Schlaugen <lukas.unity@web.de>
2019-03-09 20:22:22 +0100

    worked on player movement

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -5,30 +5,57 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
 
-    public float speed = 100;
-    public float rotspeed = 0.75f;
-	public Rigidbody2D rb;
+    public float speed = 20;
+    public float rotspeed = 10f;
+	Rigidbody2D rb;
 
-	public void FixedUpdate()
-	{	    
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
-	    if(Input.GetKey("w")) {
+    public void FixedUpdate()
+	{
+        float forwardspeed = (transform.InverseTransformDirection(rb.velocity)).y;
+        
+
+        if (Input.GetKey("w")) {
     	  	rb.AddForce(transform.up * speed);
   		}
 
  	   	if(Input.GetKey("s")) {
-  	      	rb.AddForce(transform.up * -speed);
+            
+            
+            if (forwardspeed>0)
+            {
+                rb.AddForce(transform.up * -speed * 0.5F);
+            }
+  	      	
     	}
 
 		if(Input.GetKey("d")) {
-    	    // Clockwise
-     	   transform.Rotate(0, 0, -rotspeed); // --> Instead of "transform.Rotate(-1.0f, 0.0f, 0.0f);"
+            // Clockwise
+            if (rb.angularVelocity > -30)
+            {
+                rb.AddForce(transform.right*rotspeed*0.5F);
+                rb.AddTorque(-rotspeed);
+            }
+            
+     	   //transform.Rotate(0, 0, -rotspeed); // --> Instead of "transform.Rotate(-1.0f, 0.0f, 0.0f);"
   		}
-
+       
  	   	if(Input.GetKey("a")) {
-  	      // Counter-clockwise
-  	      transform.Rotate(0, 0, rotspeed); // --> Instead of transform.Rotate(1.0f, 0.0f, 0.0f);
+            // Counter-clockwise
+            if (rb.angularVelocity < 30 )
+            {
+                rb.AddForce(-transform.right*rotspeed*0.5F);
+                rb.AddTorque(rotspeed);
+            }
+                
+  	      //transform.Rotate(0, 0, rotspeed); // --> Instead of transform.Rotate(1.0f, 0.0f, 0.0f);
     	}
+
+
 
 	}
 
