939d8a88345841774d969e11bf5a270dce881d0f
SilasZ <silaszimmermann@yahoo.de>
2019-03-10 02:43:19 +0100

    added max beam distance

--- Assets/PersonMovement.cs
+++ Assets/PersonMovement.cs
@@ -8,6 +8,8 @@ public class PersonMovement : MonoBehaviour
 
     public float beamRadius;
     public GameObject beam;
+    private float maxDistance = 10;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,7 +31,7 @@ public class PersonMovement : MonoBehaviour
     {
         var places = FindObjectsOfType<Place>().Where(a => !transform.parent || a.transform.parent != transform.parent.parent);
         var nearestPlace = places.OrderBy(a => Vector2.Distance(a.transform.position, transform.position)).FirstOrDefault();
-        if (nearestPlace)
+        if (nearestPlace && Vector2.Distance(nearestPlace.transform.position, transform.position) < maxDistance)
         {
             Instantiate(beam, transform.position - Vector3.forward, Quaternion.identity);
             Instantiate(beam, nearestPlace.transform.position - Vector3.forward, Quaternion.identity);
