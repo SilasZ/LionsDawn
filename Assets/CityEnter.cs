655e7e4ce4a77767393305a47b84f2dcba7acd36
Fabian <fabian.glatzel@web.de>
2019-03-15 16:18:03 +0100

    no wind in twon

---
+++ Assets/CityEnter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CityEnter : MonoBehaviour
+{
+    public AreaEffector2D wind;
+    public AudioSource theme;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            wind.enabled = false;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            wind.enabled = true;
+        }
+    }
+}
