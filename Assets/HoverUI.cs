10a1f17e3ce6fb97f5c7e45e2674c3861ceee565
Schlaugen <lukas.unity@web.de>
2019-03-18 00:54:44 +0100

    Added hovering over Humans to see name ect

---
+++ Assets/HoverUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HoverUI : MonoBehaviour
+{
+    public Text hoverText;
+    public Image background;
+
+    public void hide()
+    {
+        hoverText.enabled = false;
+        background.enabled = false;
+    }
+
+    public void show()
+    {
+        hoverText.enabled = true;
+        background.enabled = true;
+    }
+}
