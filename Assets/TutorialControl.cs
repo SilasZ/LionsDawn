4798648494aac4b43745b51a4a1e2efca11de119
Schlaugen <lukas.unity@web.de>
2019-03-18 16:05:07 +0100

    more tutorial

--- Assets/TutorialControl.cs
+++ Assets/TutorialControl.cs
@@ -5,17 +5,24 @@ using UnityEngine.UI;
 
 public class TutorialControl : MonoBehaviour
 {
-    public Text textA, TextB, textC1,textC2, textD1,textD2;
+    public Text textA, TextB, textC1,textC2, textD1,textD2, textD3, textD4, textD5, textD6, textE1, textE2, textE3, textF1, textF2;
     bool clickOnWood = false;
-    bool showStatueText = false;
+    bool personClicking = false;
+    bool putPersonInTown = false;
     public GameObject tutorialWood;
+    public PersonMovement tutorialHuman;
+    public GameObject tutorialTent;
     GameObject player;
     public LionStatues statues;
+    public GameObject skipButton;
     // Start is called before the first frame update
     void Start()
     {
         textA.enabled = true;
+        skipButton.SetActive(true);
         player = FindObjectOfType<Movement>().gameObject;
+        tutorialHuman.gameObject.SetActive(false);
+        tutorialTent.SetActive(false);
     }
 
 
@@ -35,19 +42,62 @@ public class TutorialControl : MonoBehaviour
             TextB.enabled = true;
         }
 
-        if (!tutorialWood&&!showStatueText)
+        if (!tutorialWood&&!personClicking)
         {
             TextB.enabled = false;
             textC1.enabled = true;
             textC2.enabled = true;
+
+            skipButton.SetActive(false);
         }
-        if (showStatueText)
+
+        if (personClicking&&!tutorialHuman.GetComponentInParent<Movement>()&& !tutorialHuman.GetComponentInParent<Place>())
         {
             textC1.enabled = false;
             textC2.enabled = false;
             textD1.enabled = true;
             textD2.enabled = true;
+            textD3.enabled = true;
+            textD4.enabled = true;
+            textD5.enabled = true;
+            textD6.enabled = true;
+
+            tutorialTent.SetActive(true);
+            tutorialHuman.gameObject.SetActive(true);
+
         }
+
+        if (tutorialHuman.GetComponentInParent<Movement>()&&!putPersonInTown)
+        {
+            putPersonInTown = true;
+            textD1.enabled = false;
+            textD2.enabled = false;
+            textD3.enabled = false;
+            textD4.enabled = false;
+            textD5.enabled = false;
+            textD6.enabled = false;
+            textE1.enabled = true;
+            textE2.enabled = true;
+            textE3.enabled = true;
+        }
+
+        if(tutorialHuman.GetComponentInParent<Place>()&& !tutorialHuman.GetComponentInParent<Movement>())
+        {
+            textD1.enabled = false;
+            textD2.enabled = false;
+            textD3.enabled = false;
+            textD4.enabled = false;
+            textD5.enabled = false;
+            textD6.enabled = false;
+            textE1.enabled = false;
+            textE2.enabled = false;
+            textE3.enabled = false;
+            Destroy(tutorialTent);
+
+            textF1.enabled = true;
+            textF2.enabled = true;
+        }
+
 
         if (statues.GotClickedFirstTime())
         {
@@ -58,6 +108,14 @@ public class TutorialControl : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        showStatueText = true;
+        personClicking = true;
+        skipButton.SetActive(false);
+    }
+
+    public void SkipTutorial()
+    {
+        Destroy(tutorialTent);
+        Destroy(tutorialHuman.gameObject);
+        Destroy(this.gameObject);
     }
 }
