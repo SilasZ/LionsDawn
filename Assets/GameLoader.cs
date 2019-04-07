2c2f2c97200a0bf4c1a35fb563c32bb643d72abb
Schlaugen <lukas.unity@web.de>
2019-04-08 01:20:16 +0200

    added loading screen

---
+++ Assets/GameLoader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameLoader : MonoBehaviour
+{
+    public Scene s;
+    // Start is called before the first frame update
+    void Update()
+    {
+        if(Time.time>0.5)
+        SceneManager.LoadScene("SampleScene",LoadSceneMode.Single);
+    }
+}
